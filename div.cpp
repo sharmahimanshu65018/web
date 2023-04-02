da67f66a3eb7d3f652bc5c3887fd39fc76f90caf
himanshusharama <sharmahimanshu65018@gmail.com>
2023-04-02 13:02:12 +0530

    div

--- div.cpp
+++ div.cpp
@@ -1,23 +1,20 @@
-#include <bits/stdc++.h> 
+#include <iostream>
 using namespace std;
- 
+
 void fast_as_fuck()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL); cout.tie(NULL); 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 #ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
 }
-
-
-
-
 
 int main()
 {
     fast_as_fuck();
-    int a=6/5;
+    int a = 6 / 5;
     return 0;
 }
