23a32870228950ae6563aa50e520f3512b4f0c8e
himanshusharama <sharmahimanshu65018@gmail.com>
2023-04-02 13:01:07 +0530

    div

---
+++ div.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+void fast_as_fuck()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL); 
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+
+
+int main()
+{
+    fast_as_fuck();
+    int a=6/5;
+    return 0;
+}
